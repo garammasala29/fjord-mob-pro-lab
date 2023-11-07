1b30cd98168315d14c23f1b4d6d2f4c1738321ff
garammasala29 <kmasa1569@gmail.com>
2023-11-07 21:14:01 +0900

    feat: 新規メモ作成機能を追加
    TODO
    - textareaのメモ反映
    - メモの削除
    
    Co-authored-by: haruguchi <haruguchi-yuma@users.noreply.github.com>
    Co-authored-by: Yusuke Sekiguchi <sadanora@users.noreply.github.com>
    Co-authored-by: rira <rira100000000@users.noreply.github.com>

--- field04/react-memo-app/src/App.tsx
+++ field04/react-memo-app/src/App.tsx
@@ -6,15 +6,46 @@ type Note = {
   content: string;
 };
 
-const notes: Note[] = [
+const defaultNotes: Note[] = [
   { id: 1, title: 'title1', content: 'memo1' },
   { id: 2, title: 'title2', content: 'memo2' }
 ];
 
 function App() {
+  const [id, setId] = useState(Math.max(...defaultNotes.map(({id}) => id)) + 1)
   const [isEditing, setIsEditing] = useState(false)
-  const handleOnClick = () => {
+  const [text, setText] = useState('')
+  const [notes, setNotes] = useState<Note[]>(defaultNotes)
 
+  // const handleOnEdit = (e: MouseEvent<HTMLButtonElement>) => {
+  //   const id = e.currentTarget?.id
+  //   console.log(id)
+  //   setIsEditing(true);
+
+  // };
+
+  const handleOnDelete = () => {
+    setIsEditing(false);
+  };
+
+  const handleOnNew = () => {
+    setIsEditing(true);
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  }
+
+  const handleOnUpdate = () => {
+    const [newTitle, ...rest] = text.split('\n');
+    const newContent = rest.join('\n');
+
+    const newNote = {id: id, title: newTitle, content: newContent};
+
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+    setText('');
+    setId((id) => id + 1);
   }
 
   return (
@@ -24,13 +55,27 @@ function App() {
         { notes.map((note) => {
           return (
             <li key={note.id}>
-              <button onClick={handleOnClick} >{ note.title }</button>
+              {/* <button onClick={handleOnEdit} id={`${note.id}`} >{ note.title }</button> */}
+              <button id={`${note.id}`} >{ note.title }</button>
             </li>
           )
         })}
       </ul>
+      <button onClick={handleOnNew}>+</button>
+      { isEditing &&
+        (
+          <div>
+            <textarea onChange={handleOnChange}></textarea>
+            <button onClick={handleOnUpdate}>更新</button>
+            <button onClick={handleOnDelete}>削除</button>
+          </div>
+        )
+      }
     </div>
   );
 }
 
 export default App;
+
+// memo
+// テキストエリアの表示は関数として切り出せそう
