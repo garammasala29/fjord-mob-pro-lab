1488f4eb909ee3a76f40fdb30722ddcd4607f53e
yuma <ikeoku.y@gmail.com>
2024-03-19 21:09:22 +0900

    ステップ3の「自動販売機は在庫の点で購入可能なドリンクのリストを取得できる」まで

--- field08/lib/suica.rb
+++ field08/lib/suica.rb
@@ -5,20 +5,29 @@ class Suica
   class InsufficientAmountError < StandardError; end
 
   attr_reader :balance
-  def initialize()
+
+  def initialize
     @balance = 0
   end
 
   def charge(amount)
     raise FloatAmountError, '小数ですよ！' if amount.to_i != amount
     raise MinimumAmountError, '100円未満ですよ！' if amount < 100
+
     @balance += amount
   end
 
   def pay(amount)
     raise FloatAmountError, '小数ですよ！' if amount.to_i != amount
     raise NegativeAmountError, '負の数ですよ！' if amount < 0
-    raise InsufficientAmountError if @balance < amount
+    raise InsufficientAmountError unless pay?(amount)
+
     @balance -= amount
+  end
+
+  private
+
+  def pay?(amount)
+    @balance >= amount
   end
 end
--- field08/spec/vending_machine_spec.rb
+++ field08/spec/vending_machine_spec.rb
@@ -1,22 +1,20 @@
 require_relative '../lib/vending_machine.rb'
 
 RSpec.describe VendingMachine do
+  let(:vending_machine) { VendingMachine.new }
+
   describe '#drink_information' do
     it '格納したドリンク情報が取得できること' do
-      vending_machine = VendingMachine.new
-      expect(vending_machine.drink_information).to eq [{name: 'コーラ', price: 120, stock: 5}]
+      expect(vending_machine.drink_information).to eq({"おしるこ"=>{:price=>80, :stock=>5}, "コーラ"=>{:price=>120, :stock=>5}, "コーンポタージュ"=>{:price=>180, :stock=>5}})
     end
   end
 
   describe '#in_stock?' do
     it '在庫が1つ以上である場合 true を返すこと' do
-      vending_machine = VendingMachine.new
-
       expect(vending_machine.in_stock?).to be true
     end
 
     it '在庫が0のである場合 falseを返すこと' do
-      vending_machine = VendingMachine.new
       vending_machine.instance_variable_set(:@drinks, [])
 
       expect(vending_machine.in_stock?).to be false
@@ -24,14 +22,39 @@ RSpec.describe VendingMachine do
   end
 
   describe '#buy' do
-    it '購入すると在庫が1つ減ること' do
-      vending_machine = VendingMachine.new
-      suica = double('suica')
+    let(:suica) { double('suica') }
+
+    before do
       allow(suica).to receive(:pay).and_return nil
+    end
+
+    it '購入すると在庫が1つ減ること' do
       vending_machine.buy('コーラ', suica)
-      coke = drink_information.find { |drink_status| drink_status[:name] == 'コーラ' }
+      coke = vending_machine.drink_information['コーラ']
+
       expect(coke[:stock]).to be 4
+    end
+
+    it '購入すると商品金額分売り上げが増えること' do
+      vending_machine.buy('コーラ', suica)
+
+      expect(vending_machine.sales).to be 120
     end
   end
 
+  describe '#in_stock_list' do
+    let(:five_cokes) { 5.times.map{ Drink.new('コーラ', 120) }}
+    let(:corn_potage) { Drink.new('コーンポタージュ', 180) }
+
+    it '在庫がある商品のリストが取得できること' do
+      vending_machine.instance_variable_set(:@drinks, [*five_cokes, corn_potage])
+
+      expect(vending_machine.in_stock_list).to eq ['コーラ', 'コーンポタージュ']
+    end
+    it '在庫がある商品が存在しないときは空のリストが取得できること' do
+      vending_machine.instance_variable_set(:@drinks, [])
+
+      expect(vending_machine.in_stock_list).to eq []
+    end
+  end
 end
