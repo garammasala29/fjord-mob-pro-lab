f1f622bf8159cc8e8d0c9bc0edac25d1bc6d0afb
rira100000000 <101010hayakawa@gmail.com>
2024-07-26 21:34:31 +0900

    メソッド検索に対応した

--- field10/rurema.rb
+++ field10/rurema.rb
@@ -1,7 +1,33 @@
 module Browser
-  def rurema(class_name)
+  def rurema(class_name, method_name = nil)
+    if Object.const_defined?(class_name)
+      klass = Object.const_get(class_name)
+    else
+      puts "#{class_name} is not found"
+      return
+    end
+    if method_name
+      if klass.instance_methods.include?(method_name)
+        category = 'i'
+        owner_class = klass.instance_method(method_name).owner
+      elsif klass.singleton_methods.include?(method_name)
+        category = 's'
+        owner_class = klass.singleton_method(method_name).owner
+      else
+        puts "#{klass}##{method_name} not found"
+        return
+      end
+
+      converted_method_name = method_name.to_s.chars.map do |c|
+        /\w/ =~ c ? c : sprintf("=%x",c.ord)
+      end.join
+
+      url = "https://docs.ruby-lang.org/ja/latest/method/#{owner_class}/#{category}/#{converted_method_name}.html"
+    else
+      url = "https://docs.ruby-lang.org/ja/latest/class/#{class_name}.html"
+    end
+
     browser_path = ENV['BROWSER']
-    url = "https://docs.ruby-lang.org/ja/latest/class/#{class_name}.html"
     case RUBY_PLATFORM
     when /linux/
       if browser_path
