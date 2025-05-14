e865e7ba741559cf6b13c3e2ce9e09c04bd5713d
sadanora <sadanora.pi@gmail.com>
2025-05-14 20:27:59 +0900

    インタプリタ回 Step 1

--- field15/rml-mini/lib/parser_step1.rb
+++ field15/rml-mini/lib/parser_step1.rb
@@ -4,5 +4,23 @@ module ParserStep1
   def self.eval(expr)
     tokens = Lexer.tokenize(expr)
     # TODO: ここに実装を書く
+    # opによって条件分岐する
+    # "+"がきたら　〜
+    # "-"がきたら ~
+    # "*" がきたら ~
+    # "/" がきたら ~
+    lhs, op, rhs = tokens
+    case op
+    when '+'
+      lhs + rhs
+    when '-'
+      lhs - rhs
+    when '*'
+      lhs * rhs
+    when '/'
+      lhs / rhs
+    else
+      raise "Unknown operator: #{op}"
+    end
   end
 end
