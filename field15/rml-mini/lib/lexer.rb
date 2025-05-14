31f88abf1a4bafedd31c13e6be4492b605203867
sadanora <sadanora.pi@gmail.com>
2025-05-14 21:04:06 +0900

    Step 2 Lexerまで

--- field15/rml-mini/lib/lexer.rb
+++ field15/rml-mini/lib/lexer.rb
@@ -1,12 +1,89 @@
-class Lexer
-  def self.tokenize(expr)
-    # step1用の Lexer
-    # この実装では後々困るので、アップデートする必要がある
-    tokens = expr.strip.split
-    lhs = tokens[0].to_i
-    op = tokens[1]
-    rhs = tokens[2].to_i
+class Token
+  attr_reader :type, :value
 
-    [lhs, op, rhs]
+  def initialize(type, value = nil)
+    @type = type
+    @value = value
   end
 end
+
+class Lexer
+  def initialize(input)
+    @input = input
+    @index = 0
+  end
+
+  def next_token
+    skip_whitespace
+    return Token.new(:eol) if eol?
+
+    case current_char
+    when /\d/ then read_number
+    when '+'
+      advance
+      Token.new(:plus)
+    when '-'
+      advance
+      Token.new(:minus)
+    when '*'
+      advance
+      Token.new(:asterisk)
+    when '/'
+      advance
+      Token.new(:slash)
+    else
+      raise "予期せぬトークンです！： #{current_char}"
+    end
+  end
+
+  private
+
+  def skip_whitespace
+    advance while current_char&.match?(/\s/)
+  end
+
+  def current_char
+    @input[@index]
+  end
+
+  def advance
+    @index += 1
+  end
+
+  def read_number
+    start_index = @index
+    advance while current_char&.match?(/\d/)
+
+    Token.new(:int, @input[start_index...@index].to_i)
+  end
+
+  def eol?
+    @index >= @input.size
+  end
+end
+
+__END__
+'+' → type :plus, value=nil
+'-' → type :minus, value = nil
+
+
+1 → :int, value = 1
+文の終わり
+   :eol, value = nil
+
+1      + 2
+
+
+0123456789
+123 + 345 + 3
+
+Token.new(:int,input[6..8])
+
+0123456789
+1 + 2 + 3
+
+6を読む→+とわかる→インデックスを1増やす
+index = 7
+input.size = 8
+
+index + 1 == input.size
--- field15/rml-mini/lib/parser_step1.rb
+++ field15/rml-mini/lib/parser_step1.rb
@@ -3,12 +3,7 @@ require_relative 'lexer'
 module ParserStep1
   def self.eval(expr)
     tokens = Lexer.tokenize(expr)
-    # TODO: ここに実装を書く
-    # opによって条件分岐する
-    # "+"がきたら　〜
-    # "-"がきたら ~
-    # "*" がきたら ~
-    # "/" がきたら ~
+
     lhs, op, rhs = tokens
     case op
     when '+'
