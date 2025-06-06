4b3b814621c83c9e1004ec7526a2ef2b62e378ec
haruguchi-yuma <ikeoku.y@gmail.com>
2025-06-06 20:13:27 +0900

    parserの処理追加

---
+++ field15/rml-mini/lib/parser_step6.rb
@@ -0,0 +1,178 @@
+## 実装方針
+# statementにif文のparseを追加
+# if文のブロックの中の復文のparseを追加
+# 比較式のparseメソッド追加
+# factorでBooleanの真理値をparseする
+
+require_relative 'lexer'
+require_relative 'node'
+
+class ParserStep6
+  def self.parse(input)
+    new(input).parse
+  end
+
+  def initialize(input)
+    @lexer = Lexer.new(input)
+    @current_token = @lexer.next_token
+  end
+
+  def parse
+    result = statement
+
+    if @current_token.type != :eol
+      raise "Unexpected token: #{@current_token.value}"
+    end
+
+    result
+  end
+
+  private
+
+  def statement
+    case @current_token.type
+    when :if
+      if_statement
+    when :identifier
+      if peek_next_token.type == :equals # 変数代入
+        var_name = @current_token.value
+        consume(:identifier)
+        consume(:equals)
+        value = comparison
+
+        Node::Assignment.new(var_name, value)
+      else
+        comparison
+      end
+    else
+      comparison
+    end
+  end
+
+  def if_statement # if < condition > { } [else-if < elsif_condition > { elsif_body } else-if <> {} else {}]
+    consume(:if)
+    consume(:less)
+    condition = comparison
+    consume(:greater)
+    consume(:l_brace)
+    then_body = statements
+    consume(:r_brace)
+
+    else_ifs = []
+    while @current_token.type == :else_if
+      consume(:else_if)
+      consume(:less)
+      elseif_condition = comparison
+      consume(:greater)
+      consume(:l_brace)
+      elsif_body = statements
+      consume(:r_brace)
+
+      else_ifs << { condition: elseif_condition, body: elsif_body}
+    end
+
+    else_body =
+      if @current_token.type == :else
+        consume(:else)
+        consume(:l_brace)
+        result = statements
+        consume(:r_brace)
+
+        result
+      end
+
+    Node::IfStatement.new(condition, then_body, else_ifs, else_body)
+  end
+
+  def statements
+    statements = []
+
+    # '}' or EOL まで statementを読む
+    while @current_token.type != :r_brace && @current_token.type != :eol
+      statements << statement
+    end
+
+    statements.size == 1 ? statements.first : Node::Block.new(statements)
+  end
+
+  def comparison
+    result = expr
+
+    while %i[equal_equal not_equal less greater equal_less equal_greater].include?(@current_token.type)
+      break if @current_token.type == :greater && peek_next_token.type == :l_brace
+      op = @current_token.type
+      consume(op)
+      rhs = expr
+
+      result = Node::ComparisonOp.new(result, op, rhs)
+    end
+
+    result
+  end
+
+  def expr
+    result = term
+
+    while %i[plus minus].include?(@current_token.type)
+      op = @current_token.type
+      consume(op)
+      rhs = term
+
+      result = Node::BinaryOp.new(result, op, rhs)
+    end
+
+    result
+  end
+
+  def term
+    result = factor
+
+    while %i[asterisk slash].include?(@current_token.type)
+      op = @current_token.type
+      consume(op)
+      rhs = factor
+
+      result = Node::BinaryOp.new(result, op, rhs)
+    end
+
+    result
+  end
+
+  def factor
+    case @current_token.type
+    when :int
+      value = @current_token.value
+      consume(:int)
+
+      Node::Integer.new(value)
+    when :true then consume(:true); Node::Boolean.new(true)
+    when :false then consume(:false); Node::Boolean.new(false)
+    when :identifier
+      name = @current_token.value
+      consume(:identifier)
+
+      Node::Variable.new(name)
+    when :l_paren
+      consume(:l_paren)
+      result = comparison # かっこの中で比較演算使える
+      consume(:r_paren)
+
+      result
+    else
+      raise "Unexpected token: #{@current_token.value}"
+    end
+  end
+
+  # 次のトークンを消費せずに確認するヘルパーメソッド
+  def peek_next_token
+    @lexer.peek_token
+  end
+
+  def consume(expected_type)
+    if @current_token.type != expected_type
+      raise "Expected #{expected_type}, got: #{@current_token.type}"
+    end
+
+    @current_token = @lexer.next_token
+  end
+end
